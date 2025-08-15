cc93765683478f6845e3057af35ddceaa87b2d23
parham-ghasemi <parham.ghasemi.1388@gmail.com>
2025-08-15 18:44:50 +0330

    feat: dashboard title

--- client/src/pages/admin/Dashboard.tsx
+++ client/src/pages/admin/Dashboard.tsx
@@ -59,7 +59,8 @@ const cards = [
 
 const Dashboard = () => {
   return (
-    <div className="flex flex-col items-center" dir="rtl">
+    <div className="flex flex-col items-center gap-8" dir="rtl">
+      <h3 className="text-2xl font-bold">اتوماسیون اداری هنرستان</h3>
       <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 max-w-4xl w-full auto-rows-fr justify-center ">
         {cards.map((card, index) => (
           <DashboardCard
