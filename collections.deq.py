de1f520f7e763263fe2d107cfc2c10299f044a04
Programming-instructor <mr.vosooghi.co@gmail.com>
2025-10-30 18:37:44 +0330

    second commit

---
+++ collections.deq.py
@@ -0,0 +1,7 @@
+# collections.dequecollections.deque
+from collections import deque
+
+dq = deque([1, 2, 3])
+dq.append(4)   # افزودن به انتهای deque
+print(dq)
+
---
+++ numpy_method.py
@@ -0,0 +1,6 @@
+# numpy.array
+import numpy as np
+
+arr = np.array([1, 2, 3])
+arr = np.append(arr, 4)   # آرایه جدید ساخته می‌شود
+print(arr)
